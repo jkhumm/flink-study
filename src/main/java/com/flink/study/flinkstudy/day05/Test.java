79fdd4214c84ae105c7f6807043cf2ccc15420c8
humingming <1158139789@qq.com>
2024-01-17 23:55:39 +0800

    flink 学习第5天  水位线

--- src/main/java/com/flink/study/flinkstudy/day05/Test.java
+++ src/main/java/com/flink/study/flinkstudy/day05/Test.java
@@ -2,16 +2,16 @@ package com.flink.study.flinkstudy.day05;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
-import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 
 import java.sql.Timestamp;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Random;
 
@@ -64,24 +64,26 @@ public class Test {
 
         DataStreamSource<TimeObj> source = env.addSource(new MyRandomIntSource());
 
-        // 添加watermark（时间水位线） ，允许窗口稍后关闭
-        DataStream<TimeObj> withWaterMark = source.assignTimestampsAndWatermarks(
-                // 创建一个设置延迟时间的对象，设置延迟时间为3秒，可能会丢失最后1s数据，设置时短但同时服务器响应会更好
-                new BoundedOutOfOrdernessTimestampExtractor<TimeObj>(Time.seconds(3)) {
-                    @Override
-                    public long extractTimestamp(TimeObj obj) {
-                        return obj.getNum();
-                    }
-                });
+        // 添加watermark（时间水位线） ，允许窗口稍后关闭  过期方法建议下面这个
+//        DataStream<TimeObj> withWaterMark = source.assignTimestampsAndWatermarks(
+//                // 创建一个设置延迟时间的对象，设置延迟时间为3秒，可能会丢失最后1s数据，设置时短但同时服务器响应会更好
+//                new BoundedOutOfOrdernessTimestampExtractor<TimeObj>(Time.seconds(3)) {
+//                    @Override
+//                    public long extractTimestamp(TimeObj obj) {
+//                        return obj.getNum();
+//                    }
+//                });
+//        //在水位线的对象上，去引用窗口计算的概念
+//        DataStream<Integer> sumStream = withWaterMark.flatMap((TimeObj ob, Collector<Integer> out) -> {
+//            out.collect(ob.getNum());
+//        }).returns(Integer.class).windowAll(TumblingProcessingTimeWindows.of(Time.seconds(5))).sum(0);
 
-        //在水位线的对象上，去引用窗口计算的概念
-        DataStream<Integer> sumStream = withWaterMark.flatMap((TimeObj ob, Collector<Integer> out) -> {
+        // 创建一个水位线的对象
+        WatermarkStrategy<TimeObj> strategy = WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(3));
+        source.assignTimestampsAndWatermarks(strategy).flatMap((TimeObj ob, Collector<Integer> out) -> {
             out.collect(ob.getNum());
-        }).returns(Integer.class).windowAll(TumblingProcessingTimeWindows.of(Time.seconds(5))).sum(0);
-        sumStream.print();
+        }).returns(Integer.class).windowAll(TumblingProcessingTimeWindows.of(Time.seconds(5))).sum(0).print();
         env.execute();
-
-
     }
 
 
